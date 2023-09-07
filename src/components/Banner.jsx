ba37c8b96763b6c6dbf28c250351f44ebcb29e92
karmegamm <karmegamm@anjaconline.org>
2023-09-07 20:35:31 +0530

    fourth

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,40 +5,74 @@ import {fadeIn} from "../variants"
 import Image from "../assets/avatar.svg"
 import { social } from '../Data';
 import {Link} from 'react-scroll'
+import { GoRocket } from "react-icons/go";
+import Lottie from 'lottie-web';
+import animationData from '../data/developer.json'
 
 const Banner = () => {
   return <section id='home' className='section min-h-[85vh] lg:min-h-[78vh]'>
     <div className="container mx-auto -mt-28">
       <div className='flex flex-col  lg:flex-row lg:items-center lg:justify-between '>
         <div  className='flex-1 text-center font-secondary lg:text-left '>
-          <h1 className='text-[55px] font-bold leading-[0.7] lg:text-[65px]'>KAR <span>MEGAM</span></h1>
-            <div className='mb-3 text-[36px] lg:text-[40px] font-secondary font-semibold uppercase leading-[1]'>
-            <span>I<span className='text-accent lg:text-[50px]'>'</span>m an Aspiring  </span>
-            <TypeAnimation 
-            sequence={[
-              'Web Developer',2000,
-              'MERN',2000,
-              'Developer',2000,
-            ]}
-            speed={50}
-            className='text-accent'
-            wrapper='span'
-            repeat={Infinity}
-            />
-          </div>
-          <p className='mb-3 max-w-lg mx-auto lg:mx-0'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum cum laboriosam quia quis rerum cupiditate dignissimos officia,  nobis.</p>
-          <div className='mb-4'>
-              <Link smooth={true} to='contact' className='btn btn-lg p-3'>Contact me</Link>
-          </div>
-          <div className='mt-3 flex text-[20px] gap-x-6  mx-auto  items-center  lg:mx-0'>
+          <motion.h1 
+            variants={fadeIn('up',0.3)} 
+            initial="hidden" 
+            whileInView={'show'} 
+            viewport={{once:false,amount:0.7}} 
+            className='text-[55px] font-bold leading-[0.7] lg:text-[65px]'
+          >KAR <span>MEGAM</span></motion.h1>
+            <motion.div 
+             variants={fadeIn('up',0.4)} 
+             initial="hidden" 
+             whileInView={'show'} 
+             viewport={{once:false,amount:0.7}} 
+            className='mb-3 text-[33px] lg:text-[35px] font-secondary font-semibold uppercase leading-[1]'>
+              <span>I<span className='text-accent lg:text-[50px]'>'</span>m an Aspiring </span>
+              <TypeAnimation 
+              sequence={[
+                'Web Developer',2000,
+                'MERN Stack Developer',2000,
+                'Developer',2000,
+              ]}
+              speed={50}
+              className='text-accent'
+              wrapper='span'
+              repeat={Infinity}
+              />
+            </motion.div>
+          <motion.p 
+           variants={fadeIn('up',0.5)} 
+           initial="hidden" 
+           whileInView={'show'} 
+           viewport={{once:false,amount:0.7}} 
+          className='mb-4 max-w-lg mx-auto lg:mx-0 lg:leading-[1.5] leading-[1.3]'>A Web wizard crafting caffeine into code,sprinkling creativity and pixel-perfect precision,enriching every online adventure.</motion.p>
+          <motion.div
+           variants={fadeIn('up',0.6 )} 
+           initial="hidden" 
+           whileInView={'show'} 
+           viewport={{once:false,amount:0.7}}  
+           className='mb-5 flex justify-center lg:justify-between items-center '>
+              <Link smooth={true} to='contact' className='btn btn-sm p-3 font-secondary text-xl flex justify-between items-center gap-x-3 '>Contact me <GoRocket className='animate-pulse'/></Link>
+          </motion.div>
+          <motion.div 
+           variants={fadeIn('up',0.7)} 
+           initial="hidden" 
+           whileInView={'show'} 
+           viewport={{once:false,amount:0.7}} 
+           className='mt-10 flex justify-center lg:justify-start text-[25px] gap-x-5  mx-auto  items-center  lg:mx-0'>
             {social.map(({href,icon},index)=>{
-              return <a className='text-white/70' key={index} href={href}>{icon}</a>
+              return <a className='text-white/70 hover:text-white' key={index} href={href}>{icon}</a>
             })}
-          </div> 
+          </motion.div> 
         </div>
-        <div className='w-[38%]'>
-          <img className='h-[80%] ' src={Image} alt={"profile"} />
-        </div>
+        <motion.div 
+        variants={fadeIn('down',0.5)} 
+        initial="hidden" 
+        whileInView={'show'} 
+        className='hidden  lg:flex flex-1 lg:mx-w-[420px] mx-auto max-w-[320px]'>
+          {/* <img className=' ' src={Image} alt={"profile"} /> */}
+
+        </motion.div>
       </div>
 
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Logo from '../assets/logo.png'
+import { SiAzurefunctions } from "react-icons/si";
+
 const Header = () => {
   return <header className='py-6'>
     <div className='container mx-auto'>
@@ -7,7 +9,7 @@ const Header = () => {
         <div className='lg:w-36 w-36 h-12  mix-blend-lighten overflow-hidden flex justify-between items-center rounded-full'> 
           <img src={Logo} className='border-teal-500 border-2  mix-blend-lighten rounded-full' alt="logo" />
         </div>
-        <button className='btn btn-sm '>Get Cv</button>
+        <button className='btn px-4 py-0.5 font-secondary lg:text-[20px] flex justify-evenly items-center gap-x-2'><SiAzurefunctions/>Get Cv</button>
       </div>
     </div>
   </header>;
