3f9916d1a28ef114b6fb7ea651174310b9902eb4
karmegamm <karmegamm@anjaconline.org>
2023-09-10 14:19:03 +0530

    new

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 // import Logo from '../assets/cover.png'
 import { SiAzurefunctions } from "react-icons/si";
+import { HiOutlineCode } from "react-icons/hi";
 // import Logo from '../assets/cover.svg'
 
 const Header = () => {
-  return <header className='py-6'>
+  return <header className='py-8'>
     <div className='container mx-auto'>
       <div className='flex justify-between items-center'>
-        <div className='lg:w-36 w-36 h-12   overflow-hidden flex justify-between items-center rounded-full'> 
+        {/*lg:w-36 w-36 h-12   overflow-hidden flex justify-between items-center rounded-full*/}
+        <div className='relative content -top-3 left-16'> 
           {/* <img src={Logo} className= 'mix-blend-lighten rounded-full' alt="logo" /> */}
-          <h3></h3>
+          <h2 className='absolute font-bold text-white/80 text-[3em]'>KM</h2>
+          <h2 className='absolute font-bold text-white/80 text-[3em]'>KM</h2>
+          <div className='absolute text-[12px] -rotate-90 left-[59px] -top-1 font-extrabold text-gradient tracking-[2px]'>DEV</div>
+          <HiOutlineCode className='absolute -left-12 -top-2 text-[45px] text-gray-500'/>
         </div>
-        <button className='btn px-4 py-0. font-secondary lg:text-[20px] flex justify-evenly items-center gap-x-2'><SiAzurefunctions/>Get Cv</button>
+        <button className='btn px-4 py-0 font-secondary lg:text-[20px] flex justify-evenly items-center gap-x-2'><SiAzurefunctions/>Get Cv</button>
       </div>
     </div>
   </header>;
