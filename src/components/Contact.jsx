598d245c87b735299db4f61d862149bfd16f7857
karmegamm <karmegamm@anjaconline.org>
2023-10-08 20:01:39 +0530

    new

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
-
+import {motion } from 'framer-motion';
+import  { } from '../variants'
 const Contact = () => {
-  return <div id='contact' className='section '>Contact</div>;
+  return <section id='contact' className='py-16 lg:section '>
+    <div className="container mx-auto">
+      <div className='flex flex-col lg:flex-row'>
+        {/* test */}
+        <div>
+          <div>
+            <h4>Get in touch</h4>
+          </div>
+        </div>
+        <div>form</div>
+      </div>
+    </div>
+    </section>
+
 };
 
 export default Contact;
